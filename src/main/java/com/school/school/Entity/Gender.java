cc7d88c76ac172ce33442a6aabeb1c1acaf9ef37
manikandan2k29 <manikandanthangapandiyan@Manikandans-MacBook-Air.local>
2024-02-29 19:43:41 +0530

    feature(3) created an api to save the Student

--- src/main/java/com/school/school/Entity/Gender.java
+++ src/main/java/com/school/school/Entity/Gender.java
@@ -1,5 +1,5 @@
 package com.school.school.Entity;
 
 public enum Gender {
-    Boy,Girl,Trans
+    BOY,GIRL,OTHER
 }
--- src/main/java/com/school/school/Entity/Student.java
+++ src/main/java/com/school/school/Entity/Student.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.time.LocalDate;
 import java.util.Date;
 
 @Entity
@@ -21,9 +22,11 @@ public class Student {
     @Column(name = "address")
     private String address;
     @Column(name = "blood_group")
-    private String bloodGroup;
+    @Enumerated(EnumType.STRING)
+    private BloodGroup bloodGroup;
     @Column(name = "date_of_birth")
-    private Date dateOfBirth;
+    private LocalDate dateOfBirth;
     @Column(name = "gender")
+    @Enumerated(EnumType.STRING)
     private Gender gender;
 }
